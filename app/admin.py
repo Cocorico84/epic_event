feb63fabc14af8c8132574dee24f527dfe48e005
Corentin <corentin.zheng@gmail.com>
2021-12-21 08:12:11 +0100

    customize admin site

--- app/admin.py
+++ app/admin.py
@@ -3,21 +3,38 @@ from django.contrib import admin
 from .models import Client, Contract, Event, Status
 
 
-class ClientAdmin(admin.ModelAdmin):
-    list_display = ()
-
-
 class ContractAdmin(admin.ModelAdmin):
-    list_display = ("id", "sales_contact", "client_name", "date_updated", "status", "amount", "payment_due",)
-    list_filter = ("date_updated", "amount",)
+    list_display = ("id", "sales", "client_name", "date_updated", "status", "amount", "payment_due",)
+    list_filter = ("date_updated",)
+
+    def client_name(self, obj):
+        return f"{obj.client.first_name} {obj.client.last_name}"
+
+    def sales(self, obj):
+        return f"{obj.sales_contact.username}"
+
+
+class ClientAdmin(admin.ModelAdmin):
+    list_display = ("id", "first_name", "last_name", "email", "phone", "company", "date_created",)
+    list_filter = ("date_created", "date_updated")
+
+
+class EventAdmin(admin.ModelAdmin):
+    list_display = ("id", "client_name", "date_created", "date_updated",)
+    list_filter = ("date_created", "date_updated")
 
     def client_name(self, obj):
         return f"{obj.client.first_name} {obj.client.last_name}"
 
 
+class StatusAdmin(admin.ModelAdmin):
+    list_display = ("id", "status",)
+    list_filter = ("status",)
+
+
 admin.site.site_header = "Epic Event Administration"
 
-admin.site.register(Client)
+admin.site.register(Client, ClientAdmin)
 admin.site.register(Contract, ContractAdmin)
-admin.site.register(Event)
-admin.site.register(Status)
+admin.site.register(Event, EventAdmin)
+admin.site.register(Status, StatusAdmin)
--- users/admin.py
+++ users/admin.py
@@ -1,7 +1,14 @@
 from django.contrib import admin
 from django.contrib.auth.models import Group
 
+
 from .models import CustomUser
 
+class CustomUserAdmin(admin.ModelAdmin):
+    list_display = ("id", "first_name", "last_name", "username", "email", "is_staff", "category",)
+    list_filter = ("last_login",)
+
 admin.site.unregister(Group)
-admin.site.register(CustomUser)
+admin.site.register(CustomUser, CustomUserAdmin)
+
+
